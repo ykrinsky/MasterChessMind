ea6c99df6ba6f48a554e63fdf71390da3bcb4739
Yuval Krinsky <ykrinsky@gmail.com>
2025-02-01 23:39:10 +0200

    Made bot.py easier for debugging and added more doc in notebook

--- bot.py
+++ bot.py
@@ -1,26 +1,34 @@
 import chess
 import torch
 
-import notes
+from train_eval_model import *
 
 
 def find_best_move(board_fen: str, eval_model):
 	board = chess.Board(board_fen)
 	best_move = None
 	best_eval = -100
+	print(f"Current board:\n{board}")
 	for move in board.legal_moves:
 		board.push(move)
 		possible_fen = board.fen()
 
-		bitboard = notes.fen_to_bitboard(possible_fen)
-		bitboard_tensor = torch.from_numpy(bitboard).to(notes.DEVICE, dtype=torch.float32)
+		bitboard = fen_to_bitboard(possible_fen)
+		bitboard_tensor = torch.from_numpy(bitboard).to(DEVICE, dtype=torch.float32)
 		new_eval = eval_model(bitboard_tensor)
-		print(f"Move: {move} give score of: {new_eval}")
+		print(f"Move: {move} give score of: {float(new_eval)}")
 		if new_eval > best_eval:
 			best_move = move
 			best_eval = new_eval
 
 		board.pop()
 
-	print(f"Best move is: {best_move}, and it gives eval of: {best_eval}")
+	print(f"Best move is: {best_move}, and it gives evaluation of: {best_eval}")
 	return best_move
+
+
+if __name__ == "__main__":
+	board_fen = "r1bqkb1r/pppp1ppp/2n2n2/4p3/3P1B2/2N5/PPP1PPPP/R2QKBNR w KQkq - 2 4"
+	eval_model = torch.load('res/model.pth')
+	eval_model.eval() 
+	find_best_move(board_fen, eval_model)
